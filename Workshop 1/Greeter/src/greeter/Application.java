1e8d2d59e7b63e7337845072fc6d60aa3c0b517e
natebakescakes <natebakescakes@gmail.com>
2018-05-24 16:15:17 +0800

    Add Greeter files

---
+++ Workshop 1/Greeter/src/greeter/Application.java
@@ -0,0 +1,13 @@
+package greeter;
+
+public class Application {
+	
+	public static void main(String[] args) {
+		Application.welcome();
+	}
+	
+	public static void welcome() {
+		System.out.println("Hello World!");
+	}
+	
+}
